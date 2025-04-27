7fd902ff62e5e7d1f8508094a35d4154741447c7
arnstadm <arnstad@gmail.com>
2025-04-27 14:07:02 +0200

    update welcome text

--- vue frontend/src/components/TheWelcome.vue
+++ vue frontend/src/components/TheWelcome.vue
@@ -7,6 +7,9 @@
 <p>Add a spot to get started</p>
 <p>
   Add a wall to that spot and then add routes.
+</p>
+<p>
+  Try out the photo analyzer to automatically produce a photo of an indoor route based on hold colors
 </p></div>
 
 </template>
